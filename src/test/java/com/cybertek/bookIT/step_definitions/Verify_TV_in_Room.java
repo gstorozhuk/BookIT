ce0952b50299c06251372ca90d1a0dcf83cecabe
Mullakhashim <mullahashimsimai@gmail.com>
2021-03-25 21:29:32 -0400

    bla bla3

--- src/test/java/com/cybertek/bookIT/step_definitions/Verify_TV_in_Room.java
+++ src/test/java/com/cybertek/bookIT/step_definitions/Verify_TV_in_Room.java
@@ -31,6 +31,7 @@ public class Verify_TV_in_Room {
 
        DB_Utilities.createConnection();
        System.out.println("123");
+       // best luck
 
       // DB_Utilities.runQuery("SELECT withtv FROM room WHERE name='kilimanjaro'").toString();
      //  String bookCountDB_Str=DB_Utilities.getFirstCellData();
